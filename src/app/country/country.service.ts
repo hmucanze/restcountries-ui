d844d5a141c39df06a213dbe1d3796568487230a
hmucanze19 <herminiomucanze@gmail.com>
2020-09-29 15:59:24 +0200

    fazendo deploy da app

--- src/app/country/country.service.ts
+++ src/app/country/country.service.ts
@@ -2,19 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Country } from './country/country';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
 
-  countryUrl = 'https://restcountries.eu';
+  countryUrl : string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.countryUrl = `${environment.countryUrl}/rest/v2`;
+  }
 
   listar(): Promise<Array<Country>> {
-
-    return this.http.get<Array<Country>>(`${this.countryUrl}/rest/v2/all`)
+    return this.http.get<Array<Country>>(`${this.countryUrl}/all`)
     .toPromise()
     .then(response => {
       const countries = response;
